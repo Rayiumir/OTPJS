4b4509f57522e7581a1481ea81b942c3bf699f25
Raymond Baghumian <swordart2050@gmail.com>
2024-05-03 01:37:44 +0330

    JavaScript for Form 4 Digit

--- js/otp.js
+++ js/otp.js
@@ -9,3 +9,44 @@
  *
  */
 
+const inputs = document.querySelectorAll("input");
+const buttons = document.querySelector("button");
+
+window.addEventListener("load", () => inputs[0].focus());
+
+inputs.forEach((input) => {
+
+    input.addEventListener("input", () => {
+
+        const currentInput = input;
+        const nextInput = input.nextElementSibling;
+
+        if (currentInput.value.length > 1 && currentInput.value.length == 2) {
+            currentInput.value = "";
+        }
+
+        if (nextInput !== null && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
+            nextInput.removeAttribute("disabled");
+            nextInput.focus();
+        }
+
+        if(!inputs[3].disabled && inputs[3].value !== "") {
+            buttons.classList.add("active");
+        }
+        else{
+            buttons.classList.remove("active");
+        }
+
+    });
+
+    input.addEventListener("keyup", (e) => {
+        if(e.key === "Backspace") {
+            if(input.previousElementSibling !== null) {
+                e.target.value = "";
+                e.target.setAttribute("disabled", true);
+                input.previousElementSibling.focus();
+            }
+        }
+    })
+
+});
