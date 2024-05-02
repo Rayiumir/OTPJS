aad95f42eb8e247876b4080de9137a9f22b2e849
Raymond Baghumian <swordart2050@gmail.com>
2024-05-03 00:39:28 +0330

    Creating Files and Folders

---
+++ js/otp.js
@@ -0,0 +1,11 @@
+/**!
+ * OTPJS v1.0.0
+ *
+ * A simple, lightweight library for Verification Form
+ *
+ * @copyright Copyright 2024 Rayiumir Limited.
+ * @author    Raymond Baghumian.
+ * @link      https://github.com/Rayiumir/OTPJS
+ *
+ */
+
